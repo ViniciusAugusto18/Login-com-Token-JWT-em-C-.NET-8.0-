f2dd38d1d6faae922aac3387a4ff9af39738ae69
Vinicius Rodrigues <vinicius.rodrigues@sisaut.com.br>
2025-09-22 18:01:45 -0300

    update name

---
+++ TokenJWT/TokenJWT/WebApplication1/Controllers/LoginController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LoginController : ControllerBase
+    {
+
+        [HttpGet(Name = "Login")]
+        public LoginController()
+        {
+
+        }
+
+    }
+}
